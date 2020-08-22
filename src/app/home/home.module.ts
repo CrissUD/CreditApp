3134c7e70565507a7e2d82bbad73f454d11f9954
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-21 19:42:11 -0500

    se agreagan features en el home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,10 +5,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { ClientQueryComponent } from './components/client-query/client-query.component';
+import { FeaturesComponent } from './components/features/features.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, BannerComponent, ClientQueryComponent],
+  declarations: [HomeComponent, BannerComponent, ClientQueryComponent, FeaturesComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
