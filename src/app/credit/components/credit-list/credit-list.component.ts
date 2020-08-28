aa421009ccf23e3cd384e0abe37880882a743675
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-28 10:34:52 -0500

    se agrega componente de lista de todos los creditos

--- src/app/credit/components/credit-list/credit-list.component.ts
+++ src/app/credit/components/credit-list/credit-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ClientService } from 'src/app/core/services/client/client.service';
+import { Credit } from 'src/app/core/models/credit.model';
+import { Client } from 'src/app/core/models/client.model';
 
 @Component({
   selector: 'app-credit-list',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credit-list.component.css']
 })
 export class CreditListComponent implements OnInit {
-
-  constructor() { }
+  clients: Client[] = [];
+  constructor(
+    private location: Location,
+    private clientService: ClientService
+  ) { }
 
   ngOnInit(): void {
+    this.fetchCredits();
+  }
+
+  fetchCredits () {
+      this.clientService.getAllClients().subscribe(clientList => {
+        this.clients = Object.keys(clientList).map((key) => {
+          const client: Client = clientList[key];
+          client.key = key;
+          if (client.credits){
+            let creditsList: Credit[] = Object.keys(client.credits).map(key => {
+              const credit: Credit = client.credits[key];
+              credit.key = key;
+              return credit;
+            });
+            client.credits = creditsList;
+          }
+          return client;
+        });
+      });
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
